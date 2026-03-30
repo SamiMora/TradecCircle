67a180db8f1a5af664479e959a663d580804129c
SamiMora <samuelberrry03@gmail.com>
2026-03-29 23:55:57 -0500

    fix cors for production

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const fs = require("fs-extra");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://69c9ff5d16676a66263655d8--sweet-lebkuchen-fe7a23.netlify.app"
+}));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
